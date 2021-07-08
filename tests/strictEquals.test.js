8a09b803b7b227d4fd20c2431731f6c0ba20d8fc
kjmatt91 <kjmatt91@gmail.com>
2021-07-08 10:31:04 -0400

    fixed debugger per karandeep's instructions

--- tests/strictEquals.test.js
+++ tests/strictEquals.test.js
@@ -1,15 +1,11 @@
-let describe = require('mocha').describe
-let it = require('mocha').it
 let expect = require('chai').expect
-let strictEquals = require('../calculation')
+let calculation = require('../strictEquals')
 
 describe('calculation', () => {
-  describe('strictEquals', () => {
-    it('returns true if the values are strictly equal to eachother', () => {
-      let num1 = 5
-      let num2 = 5
+  it('returns true if the values are strictly equal to eachother', () => {
+    let num1 = 5
+    let num2 = 5
 
-      expect(strictEquals(num1, num2)).to.equal(true)
-    })
+    expect(calculation(num1, num2)).to.equal(true)
   })
 })
